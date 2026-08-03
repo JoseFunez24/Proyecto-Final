aff25ceab8fd43c1d8f40173fc37d9ce2a75ca7d
Wilmer Gotay <wgotay16@gmail.com>
2026-08-03 12:16:19 -0600

    Mejoras en el carrusel, galeria

--- Proyecto Final/js/main.js
+++ Proyecto Final/js/main.js
@@ -17,8 +17,58 @@ topButton.className = "top-button";
 
 document.body.appendChild(topButton);
 
+// Carrusel automático
 
+const slides = document.querySelectorAll(".hero-slide");
+const indicators = document.querySelectorAll(".indicator");
 
+let current = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide=>{
+
+        slide.classList.remove("active");
+
+    });
+
+    indicators.forEach(dot=>{
+
+        dot.classList.remove("active");
+
+    });
+
+    slides[index].classList.add("active");
+
+    indicators[index].classList.add("active");
+
+}
+
+setInterval(()=>{
+
+    current++;
+
+    if(current>=slides.length){
+
+        current=0;
+
+    }
+
+    showSlide(current);
+
+},5000);
+
+indicators.forEach((dot,index)=>{
+
+    dot.addEventListener("click",()=>{
+
+        current=index;
+
+        showSlide(current);
+
+    });
+
+});
 
 
 window.addEventListener("scroll",()=>{
